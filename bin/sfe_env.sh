301f1fca052d1a50027fa253888de6db85a0efbc
= <achoudhury@tableau.com>
2017-08-17 16:06:50 -0700

    SFE Environment on Linux

---
+++ bin/sfe_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# NOTE: This script should be run EXCLUSIVELY from the SimilarFunctionEliminator directory
+# You can then run node src\run_sfe.js --file <INPUT_FILE>  > <OUTPUT_FILE>
+# You can supply the --no-compact option to not minimize whitespace.
+export NODE_PATH=$PWD/node_modules
+export PATH=$PATH:$PWD/node_modules/bin
