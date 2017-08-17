a4d274d800a7bcd720fd5bf1d1b1a979b3ffc6f9
= <achoudhury@tableau.com>
2017-08-17 16:34:46 -0700

    Example code

---
+++ example/printContainer.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <type_traits>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+
+using namespace std;
+
+template<template<class, class> class Container, typename T>
+std::string toString(const Container<T, std::allocator<T>>& container)
+{
+    ostringstream oss;
+    std::copy(container.begin(), container.end(), std::ostream_iterator<T>(oss, " "));
+    for (auto& val: container)
+    {
+      oss << val << " ";
+    }
+    return oss.str();
+}
+
+template<template<typename...> class Container, typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
+void printContainer()
+{
+  Container<T, std::allocator<T>> container = {1, 2, 3, 4, 5};
+  std::cout << toString(container) << std::endl;
+}
+
+template<template<typename...> class Container>
+void printContainers()
+{
+  printContainer<Container, int>();
+  printContainer<Container, float>(); 
+  printContainer<Container, long>(); 
+  printContainer<Container, double>(); 
+  printContainer<Container, unsigned int>();
+  printContainer<Container, unsigned long>();
+  printContainer<Container, short>();
+  printContainer<Container, unsigned short>();
+  printContainer<Container, char>();
+  printContainer<Container, unsigned char>();
+  printContainer<Container, long long>();
+  printContainer<Container, long double>();
+}
+
+int main()
+{
+  printContainers<vector>(); 
+  printContainers<list>();
+  printContainers<deque>();
+
+  return 0;
+}
---
+++ example/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Run script that demonstrates the effects of
+# Similar Function Elimination
+#
+# NOTE: You will need to be able to run
+# Emscripten, i.e., emcc should be in your path
+# and the EMSCRIPTEN and LLVM variables should be set.
+#
+export NODE_PATH=$PWD/../node_modules
+export PATH=$PATH:$PWD/../node_modules/bin
+
+echo "Running Emscripten with full optimizations turned on - writing to printContainer.js"
+emcc --std=c++11 -O3 ./printContainer.cpp -o printContainer.js
+
+echo "Now running SFE on printContainer.js. Writing to printContainer_reduced.js"
+node ../src/run_sfe.js --file printContainer.js > printContainer_reduced.js
+
+echo "Printing out old and new printContainer.js"
+ls -l printContainer*.js
+
