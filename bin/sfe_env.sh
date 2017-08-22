a1ed23012ff94f34cdb7b849fbbf409c63d7f333
= <achoudhury@tableau.com>
2017-08-22 14:50:01 -0700

    Get unit tests working on Linux

--- bin/sfe_env.sh
+++ bin/sfe_env.sh
@@ -4,4 +4,4 @@
 # You can then run node src\run_sfe.js --file <INPUT_FILE>  > <OUTPUT_FILE>
 # You can supply the --no-compact option to not minimize whitespace.
 export NODE_PATH=$PWD/node_modules
-export PATH=$PATH:$PWD/node_modules/bin
+export PATH=$PATH:$PWD/node_modules/.bin
